eb31807b5c52ff40613fe1d4168fb6078eb470bd
S7K <sala7khaledsk@gmail.com>
2019-11-02 19:34:41 +0200

    added signUp, addMedicine and forgetPassword Activities

--- app/src/main/java/view/activity/ForgetPasswordActivity.java
+++ app/src/main/java/view/activity/ForgetPasswordActivity.java
@@ -1,6 +1,7 @@
 package view.activity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.s7k.doctroid.R;
 
@@ -9,11 +10,12 @@ import view.base.BaseActivity;
 public class ForgetPasswordActivity extends BaseActivity {
 
     public ForgetPasswordActivity() {
-        super(R.layout.activity_forget_password, false);
+        super(R.layout.activity_forget_password, true);
     }
 
     @Override
     protected void doOnCreate(Bundle bundle) {
+        toolbarBackImageView.setVisibility(View.VISIBLE);
 
     }
 }
--- app/src/main/java/view/activity/LoginActivity.java
+++ app/src/main/java/view/activity/LoginActivity.java
@@ -1,6 +1,10 @@
 package view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.s7k.doctroid.R;
 
@@ -11,9 +15,35 @@ public class LoginActivity extends BaseActivity {
     public LoginActivity() {
         super(R.layout.activity_login, false);
     }
+    
+    EditText username,password;
+    TextView login, forgetPassword, createAccount;
 
     @Override
     protected void doOnCreate(Bundle bundle) {
+
+        initializeComponents();
+        setListeners();
+    }
+
+    private void initializeComponents() {
+        username = findViewById(R.id.login_username_ET);
+        password = findViewById(R.id.login_password_ET);
+        login = findViewById(R.id.login_login_TV);
+        forgetPassword = findViewById(R.id.login_forgetPassword_TV);
+        createAccount = findViewById(R.id.login_createAccount_TV);
+    }
+
+    private void setListeners() {
+        login.setOnClickListener(view -> {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        });
+        forgetPassword.setOnClickListener(view -> {
+            startActivity(new Intent(LoginActivity.this, ForgetPasswordActivity.class));
+        });
+        createAccount.setOnClickListener(view -> {
+            startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+        });
     }
 
 }
