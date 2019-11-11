5884a032084b2646c38e7cd89dd297257cce68c7
S7K <sala7khaledsk@gmail.com>
2019-11-11 17:24:06 +0200

    waveView has been added to SplashActivity

--- app/src/main/java/view/activity/SplashActivity.java
+++ app/src/main/java/view/activity/SplashActivity.java
@@ -24,12 +24,13 @@ public class SplashActivity extends BaseActivity {
     @Override
     protected void doOnCreate(Bundle bundle) {
 
+        view = findViewById(R.id.view);
         waveLoading();
         new Handler().postDelayed(this::navigateToLogin, Constants.SPLASH_TIME_OUT);
     }
 
     private void waveLoading() {
-        view = findViewById(R.id.view);
+
         WaveDrawable colorWave = new WaveDrawable(this, R.drawable.back_circle);
         view.setBackground(colorWave);
         colorWave.setIndeterminate(true);
