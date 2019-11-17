2728590525efdabfca16d006596446509694761b
S7K <sala7khaledsk@gmail.com>
2019-11-17 02:17:08 +0200

    SignUpActivity Has been added!

--- app/src/main/java/view/activity/ForgetPasswordActivity.java
+++ app/src/main/java/view/activity/ForgetPasswordActivity.java
@@ -2,6 +2,7 @@ package view.activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.s7k.doctroid.R;
 
@@ -12,10 +13,13 @@ public class ForgetPasswordActivity extends BaseActivity {
     public ForgetPasswordActivity() {
         super(R.layout.activity_forget_password, true);
     }
+    EditText email;
 
     @Override
     protected void doOnCreate(Bundle bundle) {
         toolbarBackImageView.setVisibility(View.VISIBLE);
+        toolbarTextView.setText("Forget Password");
+        toolbarTextView.setTextSize(23);
 
     }
 }
