69c1d5a347daee025b8aea6b9cb85531db2d8894
S7K <sala7khaled.s7k@gmail.com>
2020-05-08 20:27:38 +0200

    Make Signup form scrollable

--- app/src/main/java/view/category/ResultActivity.java
+++ app/src/main/java/view/category/ResultActivity.java
@@ -1,7 +1,12 @@
 package view.category;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.s7k.doctroid.R;
 
@@ -19,6 +24,16 @@ public class ResultActivity extends BaseActivity {
     protected void doOnCreate(Bundle bundle) {
         toolbarTextView.setText(R.string.medical_cv);
         toolbarBackImageView.setVisibility(View.VISIBLE);
+
+        if (ContextCompat.checkSelfPermission(ResultActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(ResultActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED )
+        {
+            ActivityCompat.requestPermissions(ResultActivity.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+        }
 
         callAPI();
         initializeComponents();
